9e3f093f787c05415f245736896f1b817aef5cdc
pieos-pi <pieos.builder.pi@gmail.com>
2020-05-13 19:55:02 +0900

    site config data, custom icons (font-awesome)

---
+++ config/CustomIcons.jsx
@@ -0,0 +1,29 @@
+import makeFAIcon from "utils/makeFAIcon";
+
+import {
+  faFilePdf,
+  faChartPie,
+  faCoins,
+  faMoneyBillAlt,
+  faShieldAlt,
+  faUserShield,
+  faPiggyBank,
+  faRocket,
+  faNetworkWired,
+  faShoppingCart,
+  faLaptop,
+  faLock } from "@fortawesome/free-solid-svg-icons";
+
+export const FilePdfIcon = makeFAIcon(faFilePdf);
+export const ChartPieIcon = makeFAIcon(faChartPie);
+export const CoinsIcon = makeFAIcon(faCoins);
+export const MoneyBillAltIcon = makeFAIcon(faMoneyBillAlt);
+export const ShieldAltIcon = makeFAIcon(faShieldAlt);
+export const UserShieldIcon = makeFAIcon(faUserShield);
+export const PiggyBankIcon = makeFAIcon(faPiggyBank);
+export const RocketIcon = makeFAIcon(faRocket);
+export const NetworkWiredIcon = makeFAIcon(faNetworkWired);
+
+export const ShoppingCartIcon = makeFAIcon(faShoppingCart);
+export const LaptopIcon = makeFAIcon(faLaptop);
+export const LockIcon = makeFAIcon(faLock);
---
+++ config/site.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: "PIEOS - Crypto-Backed Stablecoin PIE on EOS",
+  keywords: [
+    "gatsby",
+    "gatsbyjs",
+    "landing page",
+    "landing",
+    "i18n",
+    "eslint",
+    "bootstrap",
+    "startbootstrap-agency",
+  ],
+  description: "PIEOS - crypto-backed stablecoin PIE providing optional privacy-protecting transaction feature and interest-bearing savings accounts on EOSIO blockchain",
+  author: "pieos-pi",
+  trackingId: "UA-119418003-5",
+  defaultLang: "en",
+  langTextMap: {
+    en: "English",
+    ko: "한국어",
+  },
+};
