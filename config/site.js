8078ea0c3b817577f5ae029500234d978f43b02f
pieos-pi <pieos.builder.pi@gmail.com>
2020-07-12 10:24:13 +0900

    GA tracking id updated

--- config/site.js
+++ config/site.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
   description: "PIEOS - crypto-backed stablecoin PIE providing optional privacy-protecting transaction feature and interest-bearing savings accounts on EOSIO blockchain",
   author: "pieos-pi",
-  trackingId: "UA-172431084-1",
+  trackingId: "UA-172440769-1",
   defaultLang: "en",
   langTextMap: {
     en: "English",
